a899e1f573f8b68685d53cbce5350382fa04ae88
Chris Eckert​ <christopher.k.eckert@gmail.com>
2015-11-09 14:32:18 -0800

    adding monkey and test dump params

--- app/controllers/call_tracking_controller.rb
+++ app/controllers/call_tracking_controller.rb
@@ -2,7 +2,7 @@ class CallTrackingController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def forward_call
-    Rails.logger.debug params.to_string
+    Rails.logger.debug params.to_s
     lead = Lead.create(lead_params)
     render text: twilio_response.text
   end
