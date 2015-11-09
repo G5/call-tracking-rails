cebb73f9c25ffc8a34812943d1e43600df7a3b45
Chris Eckert​ <christopher.k.eckert@gmail.com>
2015-11-09 14:39:46 -0800

    add call end

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
   post 'call-tracking/forward-call' => 'call_tracking#forward_call', as: :forward_call
-  post 'call-tracking/call-end' => 'call_tracking#call-end', as: :end_call
+  post 'call-tracking/call-end' => 'call_tracking#call_end', as: :end_call
   get 'statistics/leads_by_source' => 'statistics#leads_by_source', as: :leads_by_source
   get 'statistics/leads_by_city' => 'statistics#leads_by_city', as: :leads_by_city
 
