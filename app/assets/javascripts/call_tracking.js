8c7bbe90f720f8d2ce0d24fbed3fd07863dc4aff
Chris Eckert​ <christopher.k.eckert@gmail.com>
2015-11-14 14:20:40 -0800

    fix charts

--- app/assets/javascripts/call_tracking.js
+++ app/assets/javascripts/call_tracking.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
   $.get('/statistics/leads_by_source/', function(data) {
-    CallTrackingGraph("#leads-by-source", data).draw();
+      var ctx = $("#leads-by-source").get(0).getContext("2d");
+      var myPieChart = new Chart(ctx).Pie(data.statistics,{
+          animateScale: true
+      });
   });
 
   $.get('/statistics/leads_by_city/', function(data) {
-    CallTrackingGraph("#leads-by-city", data).draw();
+    CallTrackingGraph("#leads-by-city", data.statistics).draw();
   });
 });
 
@@ -20,3 +23,52 @@ CallTrackingGraph = function(selector, data) {
     }
   }
 }
+
+/*
+
+$.getJSON('/statistics/leads_by_source/', function(results) {
+    results = _.map(_.zip(_.keys(results), _.values(results)), function(value) {
+        return {
+            description: value[0],
+            lead_count: value[1]
+        };
+    });
+
+    summaryByLeadSourceData = _.map(results, function(leadSourceDataPoint) {
+        return {
+            value: leadSourceDataPoint.lead_count,
+            color: 'hsl(' + (180 * leadSourceDataPoint.lead_count/ results.length)
+            + ', 100%, 50%)',
+            label: leadSourceDataPoint.description
+        };
+    });
+
+    var byLeadSourceContext = $("#pie-by-lead-source").get(0).getContext("2d");
+    var byLeadSourceChart =
+        new Chart(byLeadSourceContext).Pie(summaryByLeadSourceData);
+});
+
+$.getJSON('/statistics/leads_by_city/', function(results) {
+    results = _.map(_.zip(_.keys(results), _.values(results)), function(value) {
+        return {
+            city: value[0],
+            lead_count: value[1]
+        };
+    });
+
+    summaryByCityData = _.map(results, function(cityDataPoint) {
+        return {
+            value: cityDataPoint.lead_count,
+            color: 'hsl(' + (180 * cityDataPoint.lead_count/ results.length)
+            + ', 100%, 50%)',
+            label: cityDataPoint.city
+        };
+    });
+
+    var byCityContext = $("#pie-by-city").get(0).getContext("2d");
+    var byCityChart = new Chart(byCityContext).Pie(summaryByCityData);
+});
+
+
+
+*/
