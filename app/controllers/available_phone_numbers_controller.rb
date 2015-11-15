8ff392a8a9ccfb3a6d01f165382dc8a572dfcca7
Chris Eckert​ <christopher.k.eckert@gmail.com>
2015-11-14 20:20:11 -0800

    clean up

--- app/controllers/available_phone_numbers_controller.rb
+++
@@ -1,7 +0,0 @@
-class AvailablePhoneNumbersController < ApplicationController
-  def index
-    area_code = params["area-code"]
-    zip_code = params["zip-code"]
-    @phone_numbers = ::TwilioClient.available_phone_numbers(area_code, zip_code)
-  end
-end
